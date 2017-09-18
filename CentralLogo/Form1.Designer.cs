e5f2a6320c529c93f27dba7dac239a1f4ec1bf57
tylerJtucker <tyletuck418@ed.amdsb.ca>
2017-09-18 10:12:43 -0700

    Add project files.

---
+++ CentralLogo/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace CentralLogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buildButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buildButton
+            // 
+            this.buildButton.BackColor = System.Drawing.Color.Transparent;
+            this.buildButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.buildButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buildButton.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buildButton.Location = new System.Drawing.Point(338, 225);
+            this.buildButton.Name = "buildButton";
+            this.buildButton.Size = new System.Drawing.Size(253, 65);
+            this.buildButton.TabIndex = 0;
+            this.buildButton.Text = "Build";
+            this.buildButton.UseVisualStyleBackColor = false;
+            this.buildButton.Click += new System.EventHandler(this.buildButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(924, 509);
+            this.Controls.Add(this.buildButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buildButton;
+    }
+}
+
---
+++ CentralLogo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CentralLogo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buildButton_Click(object sender, EventArgs e)
+        {
+            buildButton.Visible = false;
+
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Green, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Red);     //sets up brushes and formGraphics
+
+           
+            formGraphics.FillEllipse(drawBrush, 250, 100, 400, 300);  //forms background
+
+            drawBrush.Color = Color.White;
+            Font drawFont = new Font("Sitka Small", 180, FontStyle.Bold);  //forms big C
+            formGraphics.DrawString("C", drawFont, drawBrush, 225, 150);
+
+            
+            Font drawFont2 = new Font("Sitka Small", 50, FontStyle.Bold);
+            formGraphics.DrawString("Rams", drawFont2, drawBrush, 335, 225);  //forms "rams"
+
+            Font drawFont3 = new Font("Sitka Small", 25, FontStyle.Bold);
+            formGraphics.TranslateTransform(585, 190);
+            formGraphics.RotateTransform(90); 
+            formGraphics.DrawString("Arcade", drawFont3, drawBrush, new Rectangle());  //forms "arcade"
+            formGraphics.ResetTransform();
+        }
+    }
+}
